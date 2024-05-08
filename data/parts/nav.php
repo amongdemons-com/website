fdc4d96a5cb6b49788485bcd5f72487b774b34ac
alurosu <alurosu@gmail.com>
2024-05-08 18:33:08 +0300

    FAQ and nav redo

--- data/parts/nav.php
+++ data/parts/nav.php
@@ -8,25 +8,19 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Collections
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/nfts/demons/">Demons</a></li>
-            <li><a class="dropdown-item disabled" href="#" aria-disabled="true">Items (soon)</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="/nfts/demons/faq/">FAQ</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li>
-              <a class="dropdown-item text-center" href="https://www.stargaze.zone/p/badkids/" target="_blank">
-                <button type="button" class="btn btn-primary btn-sm">Buy Now</button>
-              </a>
-            </li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="/nfts/demons/faq/">Collection</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/market">Stats</a>
+          <a class="nav-link" href="/nfts/demons/">Demons</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" aria-disabled="true">Items (soon)</a>
+        </li>
+        <li class="nav-item pt-1">
+          <a class="nav-link py-0" href="<?php echo $stargazeUrl; ?>" target="_blank">
+            <button type="button" class="btn btn-success btn-sm">Buy Now</button>
+          </a>
         </li>
       </ul>
       <ul class="navbar-nav d-flex">
--- nfts/demons/faq/index.php
+++ nfts/demons/faq/index.php
@@ -7,9 +7,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Among Demons</title>
-    <meta name="description" content="Testing stuff">
-    <meta name="keywords" content="tag 1, tag 2">
+    <title>Founders Collection - Among Demons NFTs</title>
+    <meta name="description" content="We converted the first demon models into NFTs. Owning a piece makes you a supporter of the Among Demons project.">
     <meta name="author" content="Among Demons">
 
     <?php require_once("../../../data/parts/includes.php"); ?>
@@ -18,76 +17,142 @@ session_start();
   <body>
     <?php require_once("../../../data/parts/nav.php"); ?>
     <main class="container">
-      <h1 style="text-align: center;">faq</h1>
-      <div class="accordion accordion-flush">
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
-              How many NFTs can be minted?
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse">
-            <div class="accordion-body">
-              <p>In this world, we have 11 types of demons, 6 models in each category and you can mint each one as follws:</p>
-
-              <table class="table table-sm text-center">
-                <tbody>
-                  <tr>
-                    <td>151</td>
-                    <td>Common</td>
-                    <td>91</td>
-                    <td>Epic</td>
-                  </tr>
-                  <tr>
-                    <td>131</td>
-                    <td>Uncommon</td>
-                    <td>71</td>
-                    <td>Legendary</td>
-                  </tr>
-                  <tr>
-                    <td>111</td>
-                    <td>Rare</td>
-                    <td>51</td>
-                    <td>Mythic</td>
-                  </tr>
-                </tbody>
-              </table>
-
-              <p>If we do the math, there are:</p>
-              <table class="table table-sm text-center">
-                <tbody>
-                  <tr>
-                    <td>Unique / Total</td>
-                    <td>66 / 6666</td>
-                  </tr>
-                </tbody>
-              </table>
+      <div class="row py-2">
+        <div class="col-md-6 col-sm-12">
+          <div id="demonsSlider" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="/nfts/demons/models/41.png" class="d-block w-100" alt="Legendary Baobaw">
+                <div class="carousel-caption d-none d-md-block p-0 bg-dark">
+                  <h5 class="my-1 py-2 text-dark-emphasis bg-dark-subtle"><span class="ad-legendary">Legendary</span> Baobaw</h5>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="/nfts/demons/models/8.png" class="d-block w-100" alt="Uncommon Gon G'ah">
+                <div class="carousel-caption d-none d-md-block p-0 bg-dark">
+                  <h5 class="my-1 py-2 text-dark-emphasis bg-dark-subtle"><span class="ad-uncommon">Uncommon</span> Gon G'ah</h5>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="/nfts/demons/models/58.png" class="d-block w-100" alt="Epic Ba Be'aga">
+                <div class="carousel-caption d-none d-md-block p-0 bg-dark">
+                  <h5 class="my-1 py-2 text-dark-emphasis bg-dark-subtle"><span class="ad-epic">Epic</span> Ba Be'aga</h5>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="/nfts/demons/models/13.png" class="d-block w-100" alt="Common Ma'Zga">
+                <div class="carousel-caption d-none d-md-block p-0 bg-dark">
+                  <h5 class="my-1 py-2 text-dark-emphasis bg-dark-subtle"><span class="ad-common">Common</span> Ma'Zga</h5>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="/nfts/demons/models/53.png" class="d-block w-100" alt="Legendary Chu Perk">
+                <div class="carousel-caption d-none d-md-block p-0 bg-dark">
+                  <h5 class="my-1 py-2 text-dark-emphasis bg-dark-subtle"><span class="ad-legendary">Legendary</span> Chu Perk</h5>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="/nfts/demons/models/27.png" class="d-block w-100" alt="Legendary Chu Perk">
+                <div class="carousel-caption d-none d-md-block p-0 bg-dark">
+                  <h5 class="my-1 py-2 text-dark-emphasis bg-dark-subtle"><span class="ad-rare">Rare</span> Vi'Zel</h5>
+                </div>
+              </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#demonsSlider" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#demonsSlider" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
           </div>
         </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-              Accordion Item #2
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
-            <div class="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <div class="col-md-6 col-sm-12 my-auto py-4">
+          <h1>Founders Collection</h1>
+          <p class="fs-5 py-2">We converted the first <a class="text-center" href="/nfts/demons/" target="_blank">demon models</a> into NFTs. Owning a piece makes you a supporter of the Among Demons project.</p>
+          
+          <a class="text-center" href="<?php echo $stargazeUrl; ?>" target="_blank">
+            <button type="button" class="btn btn-success">Buy on Stargaze</button>
+          </a>
+
+          <h2 class="text-center py-2 mt-4">FAQ</h2>
+
+          <div class="accordion accordion-flush" id="demonsFAQ">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                  What do I need to get one?
+                </button>
+              </h2>
+              <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#demonsFAQ">
+                <div class="accordion-body">
+                  <p>Before trying to buy an NFT, you'll need to create a <a href="https://cosmos.network/wallets/" target="_blank">Cosmos wallet</a>.</p>
+                  <p>Once you have that ready, it's time to get some <span class="text-warning-emphasis">STARS</span>. Go to <a href="<?php echo $stargazeUrl; ?>" target="_blank">stargaze.zone</a> and click on the [Get STARS] button situated in the top right corner of the website.</p>
+                  <p>If you need extra help, we'll gladly guide you. Join our <a href="/discord" target="_blank">discord</a> server and ask your question.<p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                  How many NFTs can be minted?
+                </button>
+              </h2>
+              <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#demonsFAQ">
+                <div class="accordion-body">
+                  <p>In this world, we have 11 types of demons, 6 models in each category and you can mint each one as follws:</p>
+                  <div class="row mb-4">
+                    <div class="col-md-6 col-sm-12 d-flex justify-content-center">
+                      <span class="w-25 text-end pe-2">151</span>
+                      <span class="w-50 ad-common">Common</span>
+                    </div>
+                    <div class="col-md-6 col-sm-12 d-flex justify-content-center">
+                      <span class="w-25 text-end pe-2">131</span>
+                      <span class="w-50 ad-uncommon">Uncommon</span>
+                    </div>
+                    <div class="col-md-6 col-sm-12 d-flex justify-content-center">
+                      <span class="w-25 text-end pe-2">111</span>
+                      <span class="w-50 ad-rare">Rare</span>
+                    </div>
+                    <div class="col-md-6 col-sm-12 d-flex justify-content-center">
+                      <span class="w-25 text-end pe-2">91</span>
+                      <span class="w-50 ad-epic">Epic</span>
+                    </div>
+                    <div class="col-md-6 col-sm-12 d-flex justify-content-center">
+                      <span class="w-25 text-end pe-2">71</span>
+                      <span class="w-50 ad-legendary">Legendary</span>
+                    </div>
+                    <div class="col-md-6 col-sm-12 d-flex justify-content-center">
+                      <span class="w-25 text-end pe-2">51</span>
+                      <span class="w-50 ad-mythic">Mythic</span>
+                    </div>
+                  </div>
+                  <p>If we do the math, there are:</p>
+                  <div class="row mb-4">
+                    <div class="col-md-6 col-sm-12 text-center">Unique / Total</div>
+                    <div class="col-md-6 col-sm-12 text-center"><span class="fw-bold text-danger-emphasis">66</span> / <span class="fw-bold text-danger-emphasis">6666</span></div>
+                  </div>
+                  <p>All NFTs are minted for the same price, despite rarity. The order is randomized.</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                  What's the price?
+                </button>
+              </h2>
+              <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#demonsFAQ">
+                <div class="accordion-body">
+                  <p class="mb-0">Each NFT is created for <span class="fw-bold text-danger-emphasis">666</span> <span class="text-warning-emphasis">STARS</span></p>
+                  <p class="text-secondary fs-6">(because ... obvious reasons)</p>
+                  <p>Of course, there is a secondary <a href="<?php echo $stargazeUrl; ?>" target="_blank">market</a> where people trade them at whatever price they like.</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-              Accordion Item #3
-            </button>
-          </h2>
-          <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
-            <div class="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-            </div>
-          </div>
+          
         </div>
       </div>
     </main>
