1da28f1bcde60356cb80207f1cf8d680b8ca67f5
alurosu <alurosu@gmail.com>
2024-05-18 15:35:31 +0300

    added analytics and logo+text

--- data/parts/includes.php
+++ data/parts/includes.php
@@ -7,6 +7,16 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JB4YLDMDQ6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JB4YLDMDQ6');
+</script>
+
 <?php
 
 $stargazeUrl = "https://www.stargaze.zone/l/stars1mesfmezthl2r592gqs9zgamdkcr5q3rhf0vuzcaal2w8kza57yfqfn55tz";
--- data/parts/nav.php
+++ data/parts/nav.php
@@ -25,7 +25,7 @@
       </ul>
       <ul class="navbar-nav d-flex justify-content-start flex-row">
         <li class="nav-item d-inline-flex">
-          <a class="nav-link icon-link px-2" href="https://twitter.com/amongdemonsgame" target="_blank">
+          <a class="nav-link icon-link px-2" href="https://x.com/amongdemonsgame" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
               <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
             </svg>
