6344ca27c19591d7d691eb6a34a1c853185a51c3
alurosu <alurosu@gmail.com>
2024-06-03 10:54:52 +0300

    added .ico to header

--- data/parts/includes.php
+++ data/parts/includes.php
@@ -1,4 +1,4 @@
-<link rel="icon" href="/data/img/amongdemons_logo_250x250.png" type="image/x-icon">
+<link rel="icon" href="/data/img/amongdemons.ico" type="image/x-icon">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link rel="stylesheet" href="/data/css/main.css">
